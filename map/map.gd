9914d04878a68790c6e8ab6b54c827373dca58d3
renosyah <syahputrareno975@gmail.com>
2025-08-07 10:48:11 +0700

    make better hex map generator

--- map/map.gd
+++ map/map.gd
@@ -6,14 +6,13 @@ const sea_tile_scene = preload("res://map/hex/sea/sea.tscn")
 const ground_tile_scene = preload("res://map/hex/ground/ground.tscn")
 const mountain_tile_scene = preload("res://map/hex/mountain/mountain.tscn")
 
-export var map_size :Vector2 = Vector2(6,6)
+export var map_size :int = 8
 export var spacing :float = 2
 
 onready var holder = $holder
 onready var input_detection = $input_detection
 onready var collision_shape = $Area/CollisionShape
 
-var _odd_tiles :Array = []
 var _obstacle_tiles :Array = []
 var _tiles :Dictionary = {} # Vector2 : Object TileData
 
@@ -35,164 +34,16 @@ class TileData:
 		tile_scene = c
 		is_odd = d
 		has_obstacle = e
-
-func _ready():
-	collision_shape.scale = Vector3(map_size.x,0,map_size.y)
-	var datas = _generate_tiles()
-	_spawn_tiles(datas)
-	_init_navigations()
-	
-func _generate_hex_shape_map():
-	var tile_datas :Array = []
-	var radius = map_size.x
-	var axial_tiles = _hexes_in_radius(radius)
-	
-	for axial in axial_tiles:
-		var id = _axial_to_offset(axial.x, axial.y)
-		var x_offset = 0
-		var is_odd :bool
 		
-		if int(id.y) % 2 != 0:
-			x_offset = 0.5
-			is_odd = true
-		
-		var pos = Vector3(id.x + x_offset, 0, id.y * 0.75) * spacing
-		var noise_value :Dictionary = _get_noise_value(id)
-		var tile_scene :PackedScene = noise_value["scene"]
-		var has_obstacle :bool =  noise_value["has_obstacle"]
-		tile_datas.append(TileData.new(id, pos, tile_scene, is_odd, has_obstacle))
-		
-	return tile_datas
-	
-func _generate_simple_shape_map():
-	var tile_datas :Array = []
-	var id :Vector2 = Vector2.ZERO
-	for x in range(-map_size.x, map_size.x + 1):
-		for y in range(-map_size.y, map_size.y + 1):
-			var x_offset = 0
-			var is_odd :bool
-			
-			if y % 2 != 0:
-				x_offset = 0.5
-				is_odd = true
-				
-			var pos = Vector3(x + x_offset , 0, y * 0.75) * spacing
-			var noise_value :Dictionary = _get_noise_value(id)
-			var tile_scene :PackedScene = noise_value["scene"]
-			var has_obstacle :bool =  noise_value["has_obstacle"]
-			tile_datas.append(TileData.new(id, pos, tile_scene, is_odd, has_obstacle))
-			id.y += 1
-			
-		id.y = 0
-		id.x += 1
-		
-	return tile_datas
-	
-func _generate_tiles() -> Array:
-	_noise.seed = 1
-	_noise.octaves = 3
-	_noise.period = 12.0
-	_noise.persistence = 0.856
-	_noise.lacunarity = 1.745
-	
-	return _generate_hex_shape_map()
-	
-func _get_noise_value(pos :Vector2) -> Dictionary:
-	var data :Dictionary = {
-		"scene" : sea_tile_scene,
-		"has_obstacle" : true,
-	}
-	var value = 2 * abs(_noise.get_noise_2dv(pos))
-	
-	if value > 0.8:
-		data["scene"] = mountain_tile_scene
-		data["has_obstacle"] = true
-		return data
-		
-	elif value > 0.2 and value < 0.8:
-		data["scene"] = ground_tile_scene
-		data["has_obstacle"] = false
-		return data
-		
-	return data
-	
-func _spawn_tiles(datas :Array):
-	var index :int = 1
-	for i in datas:
-		var data :TileData = i
-		if data.is_odd:
-			_odd_tiles.append(data.id)
-			
-		if data.has_obstacle:
-			_obstacle_tiles.append(data.id)
-		
-		var tile = data.tile_scene.instance()
-		tile.id = data.id
-		tile.nav_id = index
-		holder.add_child(tile)
-		tile.translation = data.position
-		
-		_tiles[data.id] = tile
-		_navigation_ids[data.id] = index
-		index += 1
-	
-func _init_navigations():
-	_add_point(_tiles.keys(), _navigation)
-	_connect_point(_tiles.keys(), _navigation)
-	_set_obstacle(_obstacle_tiles, _navigation)
-
-func get_navigation(start :Vector2, end :Vector2) -> PoolVector2Array:
-	return _get_navigation(start, end, _navigation)
-	
-func _get_navigation(start :Vector2, end :Vector2, nav :AStar2D) -> PoolVector2Array:
-	if not nav.has_point(_get_id(start)):
-		return PoolVector2Array([])
-		
-	if not nav.has_point(_get_id(end)):
-		return PoolVector2Array([])
-		
-	return nav.get_point_path(_get_id(start), _get_id(end))
-	
-func _add_point(groups :Array, nav :AStar2D):
-	for cell in groups:
-		nav.add_point(_get_id(cell), cell)
-		
-func _set_obstacle(groups :Array, nav :AStar2D, disabled: bool = true):
-	for cell in groups:
-		if nav.has_point(_get_id(cell)):
-			nav.set_point_disabled(_get_id(cell), disabled)
-			
-func _connect_point(groups :Array, nav :AStar2D):
-	for cell in groups:
-		var neighbors = get_adjacent_tile(cell)
-		if neighbors.empty():
-			continue
-			
-		for next_cell in neighbors:
-			if groups.has(next_cell):
-				nav.connect_points(_get_id(cell), _get_id(next_cell), false)
-				
-func _get_id(point : Vector2) -> int:
-	return _navigation_ids[point]
-	
 func get_tiles() -> Array:
 	return _tiles.values()
 	
 func get_tile(id :Vector2) -> BaseTile:
 	return _tiles[id]
 	
-func _get_directions(tile: Vector2) -> Array:
-	if _odd_tiles.has(tile):
-		return [
-			Vector2(1, 0), Vector2(1, -1), Vector2(0, -1),
-			Vector2(-1, 0), Vector2(0, 1), Vector2(1, 1)
-		]
-	else:
-		return [
-			Vector2(1, 0), Vector2(0, -1), Vector2(-1, -1),
-			Vector2(-1, 0), Vector2(-1, 1), Vector2(0, 1)
-		]
-		
+func get_navigation(start :Vector2, end :Vector2) -> PoolVector2Array:
+	return _get_navigation(start, end, _navigation)
+	
 func get_adjacent_tile(from: Vector2, radius: int = 1) -> Array:
 	var visited := {}
 	var frontier := [from]
@@ -239,15 +90,6 @@ func get_astar_adjacent_tile(from: Vector2, radius: int = 1) -> Array:
 			
 	return result
 	
-func _on_Area_input_event(camera, event, position, normal, shape_idx):
-	_click_position = position
-	input_detection.check_input(event)
-
-func _on_input_detection_any_gesture(_sig ,event):
-	if event is InputEventSingleScreenTap:
-		var tile : BaseTile = get_closes_tile(_click_position)
-		emit_signal("on_tile_click", tile)
-		
 func get_closes_tile(from :Vector3) -> BaseTile:
 	var current = holder.get_child(0)
 	for i in holder.get_children():
@@ -261,42 +103,171 @@ func get_closes_tile(from :Vector3) -> BaseTile:
 			
 	return current
 	
-func _hexes_in_radius(radius: int) -> Array:
-	var results = []
-	var center = Vector2(0, 0)
-	for r in range(0, radius + 1):
-		results += _hex_ring(center, r)
-	return results
+func _ready():
+	collision_shape.scale = Vector3(map_size,0,map_size)
+	var datas = _generate_tiles()
+	_spawn_tiles(datas)
+	_init_navigations()
 	
-func _hex_ring(center: Vector2, radius: int) -> Array:
-	if radius == 0:
-		return [center]
+func _generate_hex_shape_map():
+	var tile_datas :Array = []
+	var axial_tiles = _create_adjacent_tiles(Vector2.ZERO, map_size)
+	axial_tiles += [ Vector2.ZERO ]
+	
+	for id in axial_tiles:
+		var is_odd :bool = int(id.y) % 2 != 0
+		var x_offset:float = 0.5 if is_odd else 0
 		
-	var results = []
-	var q = center.x
-	var r = center.y
+		var pos = Vector3(id.x + x_offset, 0, id.y * 0.75) * spacing
+		var noise_value :Dictionary = _get_noise_value(id)
+		var tile_scene :PackedScene = noise_value["scene"]
+		var has_obstacle :bool =  noise_value["has_obstacle"]
+		tile_datas.append(TileData.new(id, pos, tile_scene, is_odd, has_obstacle))
+		
+	return tile_datas
 	
-	var hex = Vector2(q - radius, r + 0)
-	var directions = [
-		Vector2(1, -1), Vector2(1, 0), Vector2(0, 1),
-		Vector2(-1, 1), Vector2(-1, 0), Vector2(0, -1)
-	]
-	
-	for dir in directions:
-		for i in range(radius):
-			results.append(hex)
-			hex += dir
+func _generate_simple_shape_map():
+	var tile_datas :Array = []
+	var id :Vector2 = Vector2.ZERO
+	for x in range(-map_size, map_size + 1):
+		for y in range(-map_size, map_size + 1):
+			var is_odd :bool = int(id.y) % 2 != 0
+			var x_offset:float = 0.5 if is_odd else 0
 			
-	return results
+			var pos = Vector3(x + x_offset , 0, y * 0.75) * spacing
+			var noise_value :Dictionary = _get_noise_value(id)
+			var tile_scene :PackedScene = noise_value["scene"]
+			var has_obstacle :bool =  noise_value["has_obstacle"]
+			tile_datas.append(TileData.new(id, pos, tile_scene, is_odd, has_obstacle))
+			id.y += 1
+			
+		id.y = 0
+		id.x += 1
+		
+	return tile_datas
 	
-func _axial_to_offset(q: int, r: int) -> Vector2:
-	var col = q
-	var row = r + int((q - (q & 1)) / 2)  # odd-q
-	return Vector2(col, row)
+func _generate_tiles() -> Array:
+	_noise.seed = 1
+	_noise.octaves = 3
+	_noise.period = 12.0
+	_noise.persistence = 0.856
+	_noise.lacunarity = 1.745
 	
+	return _generate_hex_shape_map()
+	
+func _get_noise_value(pos :Vector2) -> Dictionary:
+	var data :Dictionary = {
+		"scene" : sea_tile_scene,
+		"has_obstacle" : true,
+	}
+	
+	var value = 2 * abs(_noise.get_noise_2dv(pos))
+	
+	if value > 0.8:
+		data["scene"] = mountain_tile_scene
+		data["has_obstacle"] = true
+		return data
 
+	elif value > 0.1 and value < 0.8:
+		data["scene"] = ground_tile_scene
+		data["has_obstacle"] = false
+		return data
+		
+	return data
+	
+func _spawn_tiles(datas :Array):
+	var index :int = 1
+	for i in datas:
+		var data :TileData = i
+		if data.has_obstacle:
+			_obstacle_tiles.append(data.id)
+		
+		var tile = data.tile_scene.instance()
+		tile.id = data.id
+		tile.nav_id = index
+		holder.add_child(tile)
+		tile.translation = data.position
+		
+		_tiles[data.id] = tile
+		_navigation_ids[data.id] = index
+		index += 1
+	
+func _init_navigations():
+	_add_point(_tiles.keys(), _navigation)
+	_connect_point(_tiles.keys(), _navigation)
+	_set_obstacle(_obstacle_tiles, _navigation)
+	
+func _get_navigation(start :Vector2, end :Vector2, nav :AStar2D) -> PoolVector2Array:
+	if not nav.has_point(_get_id(start)):
+		return PoolVector2Array([])
+		
+	if not nav.has_point(_get_id(end)):
+		return PoolVector2Array([])
+		
+	return nav.get_point_path(_get_id(start), _get_id(end))
+	
+func _add_point(groups :Array, nav :AStar2D):
+	for cell in groups:
+		nav.add_point(_get_id(cell), cell)
+		
+func _set_obstacle(groups :Array, nav :AStar2D, disabled: bool = true):
+	for cell in groups:
+		if nav.has_point(_get_id(cell)):
+			nav.set_point_disabled(_get_id(cell), disabled)
+			
+func _connect_point(groups :Array, nav :AStar2D):
+	for cell in groups:
+		var neighbors = get_adjacent_tile(cell)
+		if neighbors.empty():
+			continue
+			
+		for next_cell in neighbors:
+			if groups.has(next_cell):
+				nav.connect_points(_get_id(cell), _get_id(next_cell), false)
+				
+func _get_id(point : Vector2) -> int:
+	return _navigation_ids[point]
+	
+func _get_directions(tile: Vector2) -> Array:
+	if int(tile.y) % 2 != 0:
+		return [
+			Vector2(1, 0), Vector2(1, -1), Vector2(0, -1),
+			Vector2(-1, 0), Vector2(0, 1), Vector2(1, 1)
+		]
+	else:
+		return [
+			Vector2(1, 0), Vector2(0, -1), Vector2(-1, -1),
+			Vector2(-1, 0), Vector2(-1, 1), Vector2(0, 1)
+		]
+		
+func _on_Area_input_event(camera, event, position, normal, shape_idx):
+	_click_position = position
+	input_detection.check_input(event)
 
+func _on_input_detection_any_gesture(_sig ,event):
+	if event is InputEventSingleScreenTap:
+		var tile : BaseTile = get_closes_tile(_click_position)
+		emit_signal("on_tile_click", tile)
+		
 
+func _create_adjacent_tiles(from: Vector2, radius: int = 1) -> Array:
+	var visited := {}
+	var frontier := [from]
+	visited[from] = true
+
+	for step in range(radius):
+		var next_frontier := []
+		for current in frontier:
+			var directions = _get_directions(current)
+			for dir in directions:
+				var neighbor = current + dir
+				if not visited.has(neighbor):
+					visited[neighbor] = true
+					next_frontier.append(neighbor)
+		frontier = next_frontier
+		
+	visited.erase(from)
+	return visited.keys()
 
 
 
